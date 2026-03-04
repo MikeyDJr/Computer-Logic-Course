70dda9628d3e8e1ab997d58e3071ce51d0cc35e1
MikeyDJr <mdavis932@ivytech.edu>
2026-03-03 19:00:17 -0500

    Raining Python Script

---
+++ raining.py
@@ -0,0 +1,13 @@
+
+# Program: Raining Cats and Dogs
+# This program prints a sentence using variables.
+ 
+# Assign variables
+c = "cats"
+d = "dogs"
+ 
+# Create sentence
+s = "It is raining " + c + " and " + d + "."
+ 
+# Print sentence
+print(s)
